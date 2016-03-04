b2b78bc43cb52ffad252324ab709c81a517222db
Michail Alvanos <malvanos@gmail.com>
2016-03-04 17:07:53 +0200

    Added compilation flags for icc

--- src/compile.sh
+++ src/compile.sh
@@ -36,14 +36,13 @@ yasm -I.  -DARCH_X86_64=1 -f elf64 -Worphan-labels -DSTACK_ALIGNMENT=32 -DHIGH_B
 yasm -I.  -DARCH_X86_64=1 -f elf64 -Worphan-labels -DSTACK_ALIGNMENT=32 -DHIGH_BIT_DEPTH=0 -DBIT_DEPTH=8 ./asm/x86/quant-a.asm -o quant-a.o
 
 
-gcc -g -Wall -O0  -march=corei7 --std=gnu99 -DARCH_X86_64=1 -DHAVE_MMX \
+fake.sh icc  -O3 -vec-report=5 -std=c99 -DARCH_X86_64=1 -DHAVE_MMX \
     cpu-detect.o checkasm-a.o predict-a.o const-a.o pixel-a.o  sad-a.o dct-a.o dct-64.o quant-a.o trellis-64.o  cabac-a.o mc-a.o mc-a2.o deblock-a.o \
     ./asm/x86/predict-c.c ./asm/x86/mc-c.c \
     ./c_kernels/pixel.c ./c_kernels/predict.c ./c_kernels/quant.c  ./c_kernels/dct.c  ./c_kernels/mc.c ./c_kernels/deblock.c\
     main.c bench_pixel.c  bench_dct.c bench_cabac.c  bench_mc.c bench_intra.c bench_deblock.c bench_quant.c \
     bench.c cpu.c   -I./ -lm
 
-#gcc -g -Wall -O3  -std=c99 -DARCH_X86_64=1 -DHAVE_MMX cpu-detect.o checkasm-a.o predict-a.o const-a.o pixel-a.o  sad-a.o  \
-#    ./asm/x86/predict-c.c ./c_kernels/pixel.c ./c_kernels/predict.c main.c bench_pixel.c bench.c cpu.c   -I./
+#gcc -g -Wall -O3 -ftree-vectorizer-verbose=2 -march=corei7 --std=gnu99 -DARCH_X86_64=1 -DHAVE_MMX \
 
 
